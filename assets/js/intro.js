fed2407c7a3fd51beefb33b774ad7222deace096
Steven Moya <smoya@atomsoluciones.com>
2025-01-19 12:12:33 -0600

    Ajuste barra

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,13 +1,16 @@
 const scannerizza = document.getElementById('scannerizza');
 const personalizza = document.getElementById('personalizza');
 const esplora = document.getElementById('esplora');
+const body = document.getElementById('body');
 
 function avantiDiapositiva(diapositiva) {
     if (diapositiva == "personalizza") {
         scannerizza.classList.add('nascondere');
+        body.classList.add('nero');
     }
     else if (diapositiva == "esplora") {
         personalizza.classList.add('nascondere');
+        body.classList.remove('nero');
     }
 
 }
@@ -15,6 +18,7 @@ function avantiDiapositiva(diapositiva) {
 function indietroDiapositiva(diapositiva) {
     if (diapositiva == "personalizza") {
         scannerizza.classList.add('mostrare');
+        body.classList.remove('nero');
         setTimeout(function () {
             scannerizza.classList.remove("nascondere");
             scannerizza.classList.remove("mostrare");
@@ -22,6 +26,7 @@ function indietroDiapositiva(diapositiva) {
     }
     else if (diapositiva == "esplora") {
         personalizza.classList.add('mostrare');
+        body.classList.add('nero');
         setTimeout(function () {
             personalizza.classList.remove("nascondere");
             personalizza.classList.remove("mostrare");
